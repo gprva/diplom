2782b038ee68b2715431aaac153971e5dd5a6782
gprva <148754074+gprva@users.noreply.github.com>
2024-05-27 15:42:01 +0600

    improved logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,13 +1,11 @@
 import "./Logo.css";
 import logoImg from '../../img/logo-gadget.png'
-import logoTxt from '../../img/logo-txt.png'
 
 export default function Logo() {
   return (
     <div className="Logo flex">
       
         <img src={logoImg} alt="logo" className="logo_img" />
-        <img src={logoTxt} alt="" className="logo_txt"/>
       
     </div>
   )
