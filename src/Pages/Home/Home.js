2c96b7812918095abf140cb72cf88b4535350811
gprva <148754074+gprva@users.noreply.github.com>
2024-05-29 10:41:23 +0600

    added new arrials

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,11 +4,10 @@ import laptop from "../../img/laptop-fr.jpeg"
 import smartwatches from "../../img/smartwatched.jpg"
 import headphones from "../../img/zakrytye.jpg"
 import phone from "../../img/phone.jpg"
-import slideImg from "../../img/phone-for-slid.avif"
-import slideImg2 from "../../img/phone2.jpg"
-import slideImg3 from "../../img/phone3.webp"
-import slideImg4 from "../../img/phone4.png"
-import slideImg1 from "../../img/phone1.png"
+import slideImg from "../../img/iphoneNew1.avif"
+import slideImg2 from "../../img/iphoneNew2.avif"
+import slideImg3 from "../../img/iphoneNew3.avif"
+import { Link } from "react-router-dom";
 
 function Home() {
   
@@ -42,7 +41,7 @@ function Home() {
                 <figcaption>
             <h3>Laptop</h3>
             <p>A laptop is a window to a world of limitless possibilities. Lorem ipsum, dolor sit amet consectetur adipisicing elit. At ipsum natus eveniet! Reprehenderit ab, id unde, saepe sequi fugit dicta quos, doloremque veniam similique laboriosam enim quo. Deserunt, reiciendis facilis</p>
-            <a href="/categories/laptop" className="home_btn">shop</a>
+            <Link to="/categories/laptop" className="home_btn">shop</Link>
                 </figcaption>			
             </figure>
           </div>
@@ -54,7 +53,7 @@ function Home() {
             <h3>Smartwatches</h3>
             <p className="selling_min_txt">"Smartwatches are not just an accessory but a technological companion that combines style and functionality on your wrist."Smartwatches are style and intelligence in one device."Smartwatches are style and intelligence in one device."Smartwatches are style and intelligence in one device</p>
             
-            <a href="/categories/watch	" className="home_btn">shop</a>
+            <Link to="/categories/watch	" className="home_btn">shop</Link>
                 </figcaption>		
             </figure>
 
@@ -71,7 +70,7 @@ function Home() {
                 <figcaption>
             <h3>Headphones</h3>
             <p>"Headphones are a portal to the world of sound, where every note comes to life, immersing you in a musical universe. "Headphones are a portal to the world of sound, where every note comes to life, immersing you in a musical universe." "Headphones are a portal to the world of sound"</p>
-            <a href="/categories/headphones" className="home_btn">shop</a>
+            <Link to="/categories/headphones" className="home_btn">shop</Link>
                 </figcaption>			
             </figure>
           </div>
@@ -83,7 +82,7 @@ function Home() {
             <h3>Phone</h3>
             <p className="selling_min_txt">"A phone is not just a means of communication, but a key to an infinite world of possibilities that fits in your pocket.""A phone is not just a means of communication, but a key to an infinite world of possibilities that fits in your pocket.""A phone is not just a means of communication</p>	
             
-            <a href="/categories/smartphone" className="home_btn">shop</a>
+            <Link to="/categories/smartphone" className="home_btn">shop</Link>
                 </figcaption>		
             </figure>
 
@@ -94,29 +93,61 @@ function Home() {
 
       </div>
     </section>
+      
 
-    <section>
+    <section className="home_body">
       <div className="container">
-        <div className="home_tit">NEW THIS WEEK</div>
-      
 
-      <div className="slid">
-      <div class="container_s">
-        <div class="box box-1"  data-text="Renji"> <img className="slider_img" src={slideImg1} alt="" /> </div>
-        <div class="box box-2"  data-text="Sora"><img className="slider_img" src={slideImg} alt="" /></div>
-        <div class="box box-3"  data-text="Kaito"><img className="slider_img_3" src={slideImg2} alt="" /></div>
-        <div class="box box-4"  data-text="Tsuki"><img className="slider_img" src={slideImg3} alt="" /></div>
-        <div class="box box-5"  data-text="Mitsui"><img className="slider_img" src={slideImg4} alt="" /></div>
-      </div>
-      </div>
+      <h2 className="home_tit">New arrival</h2>
+
+      <div className="container_h">
+      
+      <div class="card">
+            <div class="face face1">
+                <div class="content">
+                    <div class="icon">
+                      <img className="animation_img" src={slideImg} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content">
+                    <p>Apple iPhone 14 128GB 2 nanoSIM Purple Smartphone.</p>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="face face1">
+                <div class="content">
+                    <div class="icon">
+                      <img className="animation_img" src={slideImg2} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content">
+                
+                    <p>Apple iPhone 15 128GB 2 nanoSIM Purple Smartphone</p>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="face face1">
+                <div class="content">
+                    <div class="icon">
+                      <img className="animation_img" src={slideImg3} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content">
+                    <p>Apple iPhone 15 256GB 2 nanoSIM Purple Smartphone</p>
+                </div>
+            </div>
+        </div>
 
       </div>
-    </section>
-      
 
-    <section>
-      <div className="container">
-      
       </div>
     </section>
       
