39464a9dff42e17329a317ce0f42b5f22bdef58b
gprva <148754074+gprva@users.noreply.github.com>
2024-05-29 10:38:44 +0600

    fixed errors

--- src/Components/OrderForm/OrderForm.js
+++ src/Components/OrderForm/OrderForm.js
@@ -52,7 +52,8 @@ export default function OrderForm({ active, setActive }) {
               <input type="text" name="address" placeholder="Country, city, street" required />
             </label>
             <div>
-              <Button type="Button">Submit</Button>
+              <Button type="submit">Submit</Button>
+              
             </div>
           </form>
         </div>
