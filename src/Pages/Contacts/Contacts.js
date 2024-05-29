b98e36d53afaaf700f8dd20e2449d09c2d7c620f
gprva <148754074+gprva@users.noreply.github.com>
2024-05-29 10:40:44 +0600

    add link

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React from "react";
 import "./Contacts.css";
 import { FaWhatsapp } from "react-icons/fa6";
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { CgMail } from "react-icons/cg";
+import { Link } from "react-router-dom";
 
 const Contacts = () => {
   return (
     <>
-      <section className='contact_main'>
-      <div className="container">
-            <h2 className='contact_tit'>Вы можете связаться с нами</h2>
-      </div>
-    </section>
+      <section className="contact_main">
+        <div className="container">
+          <h2 className="contact_tit">Вы можете связаться с нами</h2>
+        </div>
+      </section>
 
-    <section>
-      <div className="container">
+      <section>
+        <div className="container">
+          <p className="contact_phon_tit">Контактный тел: <Link to="tel:+996 505 505 400" >+996 505 505 400</Link></p>
 
-        <p className="contact_phon_tit">Контактный тел: +996 505 505 400</p>
-        <p className="contact_phon"> <CgMail className='Whatsapp'/> <span>gaparovalola08@gmail.com</span></p>
-        <p className="contact_phon_tit">Соц. сети</p>
-        <p className="contact_phon"> <FaWhatsapp className='Whatsapp'/> <span>+996 505 505 400</span> </p>
-        <p className="contact_phon"><FaTelegramPlane  className='Whatsapp'/> <span>+996 505 505 400</span>   </p>
-        <p className="contact_phon"><FaInstagram className='Whatsapp'/> <span>gprva_</span> </p>
-        
-      </div>
-    </section>
+          <Link to="mailto:gaparovalola08@gmail.com" className="contact_phon">
+            <CgMail className="Whatsapp" />
+            <span>gaparovalola08@gmail.com</span>
+          </Link>
+
+          <p className="contact_phon_tit">Соц. сети</p>
+
+          <Link to="https://wa.me/qr/5CCLEVSIBJ43F1" className="contact_phon">
+            <FaWhatsapp className="Whatsapp" /> <span>+996 505 505 400</span>
+          </Link>
+          <Link to="https://t.me/+996505505400" className="contact_phon flex">
+            <FaTelegramPlane className="Whatsapp" />
+            <span>+996 505 505 400</span>
+          </Link>
+          <Link to="https://t.me/+996505505400" className="contact_phon">
+            <FaInstagram className="Whatsapp" /> <span>gprva_</span>
+          </Link>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Contacts
+export default Contacts;
