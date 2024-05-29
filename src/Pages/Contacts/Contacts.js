ac0dd76d42d78f84cf0776d7ad3359611a44d7bf
gprva <148754074+gprva@users.noreply.github.com>
2024-05-29 13:40:59 +0600

    add adaptablyti

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -12,7 +12,13 @@ const Contacts = () => {
       <section className="contact_main">
         <div className="container">
           <h2 className="contact_tit">Вы можете связаться с нами</h2>
+          <div className="Map">
+          </div>
         </div>
+      </section>
+
+      <section>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d735.5995417556852!2d78.38666766015602!3d42.483087066586336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38865b5c92042535%3A0x78a9de24f2cc9c3b!2sIT-Academy%20Karakol!5e0!3m2!1sen!2skg!4v1716635134251!5m2!1sen!2skg" width="100%" height="400" allowfullscreen="" loading="lazy" title='qwer' referrerpolicy="no-referrer-when-downgrade"></iframe>
       </section>
 
       <section>
